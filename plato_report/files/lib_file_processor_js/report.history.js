682fd45e8e46d4fd1746571bc6d1acdfcbe239a8
Meettya <meettya@gmail.com>
2013-04-16 21:38:07 +0400

    * fix Plato report

--- plato_report/files/lib_file_processor_js/report.history.js
+++ plato_report/files/lib_file_processor_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 14 Apr 2013 17:54:54 GMT","sloc":193,"lloc":105,"functions":18,"deliveredBugs":1.2474025587328539,"maintainability":69.8035630477496,"lintErrors":3,"difficulty":29.796703296703296}]
+__history = [{"date":"Tue, 16 Apr 2013 17:36:51 GMT","sloc":193,"lloc":105,"functions":18,"deliveredBugs":1.2474025587328539,"maintainability":69.8035630477496,"lintErrors":3,"difficulty":29.796703296703296}]
